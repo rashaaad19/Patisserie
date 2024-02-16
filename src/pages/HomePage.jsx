6db7944470686b73d7f6592ea303e7aa5cc373e1
rashaaad19 <egyoussef_2000@hotmail.com>
2024-02-16 21:58:43 +0200

    fixed css styles and added media queries

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,31 @@
+import classes from "./HomePage.module.css";
+import { RiCake3Line } from "react-icons/ri";
 
 const HomePage = () => {
   return (
-    <h1>HomePage</h1>
-  )
-}
+    <div className={classes.container}>
+      <div className={classes.textControl}>
+        <h1>Foodies for dessert recipes</h1>
+        <p>Best recipes across the internet.</p>
+      </div>
+      <div className={classes.imagesContainer}>
+        <div className={classes.imageControl}>
+          <img src="Donuts.png" />
+        </div>
+        <div className={classes.imageControl}>
+          <img src="Macaron.png" />
+        </div>
 
-export default HomePage
+        <div className={classes.imageControl}>
+          <img src="Chocolate.png" />
+        </div>
+        <div className={classes.imageControl}>
+        <RiCake3Line size={50}/>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
